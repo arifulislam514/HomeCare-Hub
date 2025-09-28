797dc53424a3abceaaafeb16642e1d5135bfbe47
arifulislam514 <ariful425616@gmail.com>
2025-09-29 00:28:42 +0600

    add frontend url

--- home_care_hub/settings.py
+++ home_care_hub/settings.py
@@ -185,8 +185,7 @@ SWAGGER_SETTINGS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5173/", #react dev server
 ]
 
 cloudinary.config(
