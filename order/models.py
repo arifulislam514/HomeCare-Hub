d4d98d31e8890425adb82387dcc37a4cacdaadfc
arifulislam514 <ariful425616@gmail.com>
2025-10-26 22:45:04 +0600

    update order model

--- order/models.py
+++ order/models.py
@@ -33,23 +33,21 @@ class CartItem(models.Model):
 
 
 class Order(models.Model):
-    NOT_PAID = 'Not Paid'
-    READY_TO_SHIP = 'Ready To Ship'
-    SHIPPED = 'Shipped'
-    DELIVERED = 'Delivered'
+    UNPAID = 'Unpaid'
+    PENDING = 'Pending'
+    COMPLETE = 'Complete'
     CANCELED = 'Canceled'
     STATUS_CHOICES = [
-        (NOT_PAID, 'Not Paid'),
-        (READY_TO_SHIP, 'Ready To Ship'),
-        (SHIPPED, 'Shipped'),
-        (DELIVERED, 'Delivered'),
+        (UNPAID, 'Unpaid'),
+        (PENDING, 'Pending'),
+        (COMPLETE, 'Complete'),
         (CANCELED, 'Canceled')
     ]
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="orders")
     status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default=NOT_PAID)
+        max_length=20, choices=STATUS_CHOICES, default=UNPAID)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
